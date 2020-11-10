0de07025e3ae033057d73311a592f9ad72c4b877
Roshith CK <roshith.chiminiyan@comeon.com>
2020-11-10 23:44:26 +0100

    Fix to mandatory fields

--- dist/cypress-testrail-reporter.js
+++ dist/cypress-testrail-reporter.js
@@ -29,8 +29,6 @@ var CypressTestRailReporter = /** @class */ (function (_super) {
         _this.validate(reporterOptions, 'projectId');
         _this.validate(reporterOptions, 'suiteId');
         _this.validate(reporterOptions, 'typeId');
-        _this.validate(reporterOptions, 'groupId');
-        _this.validate(reporterOptions, 'filter');
         runner.on('start', function () {
             var executionDateTime = moment().format('MMM Do YYYY, HH:mm (Z)');
             var name = (reporterOptions.runName || 'ComeOn Group Automated test run') + " " + executionDateTime;
